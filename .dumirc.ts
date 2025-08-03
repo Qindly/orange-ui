bde2a7ce7723c36651110dd2bd13370df2ec6b54
Qindly <1392631298@qq.com>
2025-08-03 20:18:38 +0800

    ci: 添加了CI/CD部分

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  base: '/orange-ui/',
+  publicPath: '/orange-ui/',
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'orange-ui',
@@ -16,27 +18,3 @@ export default defineConfig({
   },
 })
 
-// nav: [
-//   { title: '指南', link: '/introduce' },
-//   { title: '组件', link: '/components/message' },
-// ],
-// menu: {
-//   '/': [
-//     {
-//       title: '指南',
-//       children: [
-//         { title: '介绍', link: '/introduce' },
-//         { title: '快速开始', link: '/quick-start' },
-//         { title: '按需引入', link: '/import-on-demand' },
-//       ],
-//     },
-//     {
-//       title: '工具',
-//       children: [
-//         { title: '安装', link: '/install' },
-//         { title: '构建', link: '/build' },
-//         { title: '贡献指南', link: '/contributing' },
-//       ],
-//     },
-//   ],
-// },
